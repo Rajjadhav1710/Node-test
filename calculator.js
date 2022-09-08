34750cdc78b05c196105a032a73c9d04df50cfdd
Rajjadhav1710 <66353781+Rajjadhav1710@users.noreply.github.com>
2022-09-08 12:18:55 +0530

    initial commit

---
+++ calculator.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true})); 
+
+app.get("/",function(req,res){
+    // console.log("curr:"+__dirname);
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/",function(req,res){
+    // console.log(req.body);
+    var num1=Number(req.body.num1);
+    var num2=Number(req.body.num2);
+    var result=num1 + num2;
+    res.send("Thnks for submitting:"+result);
+});
+
+app.get("/bmicalculator",function(req,res){
+    res.sendFile(__dirname+"/bmiCalculator.html");
+});
+
+app.post("/bmicalculator",function(req,res){
+    var weight=parseFloat(req.body.weight);
+    var height=parseFloat(req.body.height);
+    var BMI = weight/(height*height);
+    res.send("Your BMI is "+ BMI);
+});
+
+app.listen(3000,function(){
+    console.log("server started on port 3000");
+})
